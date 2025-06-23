15afd26983c069b8f5f52e28284c7d1f9b5c21da
WrishavSett <wrishavsett2912@gmail.com>
2025-06-23 16:07:54 +0530

    v1.0 RAG/Chatbot, New Chat not working

--- llm_utils.py
+++ llm_utils.py
@@ -2,7 +2,7 @@ import requests
 
 def query_ollama(prompt: str, model="llama3.2:3b") -> str:
     response = requests.post(
-        "http://ollama:11434/api/generate",
+        "http://localhost:11434/api/generate",
         json={"model": model, "prompt": prompt, "stream": False}
     )
     return response.json().get("response", "Failed to get response from Ollama.")
